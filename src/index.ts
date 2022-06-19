3d1e6fa26f2fef3fea5ab3d4f20d763db9783d39
AyresMonteiro <ayresmonteiro52@gmail.com>
2022-06-19 09:04:58 -0300

    feat: adds index.ts export statements

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { ComradeAtomFilter } from "./contracts/ComradeAtomFilter";
+export { ComradeFilter } from "./contracts/ComradeFilter";
+
+export { BaseAtomFilter } from "./filters/BaseAtomFilter";
+export { CompoundFilter } from "./filters/CompoundFilter";
+export { DateFilter } from "./filters/DateFilter";
+export { NumberFilter } from "./filters/NumberFilter";
+export { StringFilter } from "./filters/StringFilter";
